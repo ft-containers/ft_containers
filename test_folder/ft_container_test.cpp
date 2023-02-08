d257867ccd04104ac2936064e5b51ca734c8a7d4
Haeun Jeong <gkdms4248@gmail.com>
2023-02-08 14:50:27 +0900

    vector unit test

--- test_folder/ft_container_test.cpp
+++ test_folder/ft_container_test.cpp
@@ -4,60 +4,83 @@
 #include <iomanip>
 
 
-// using allocator
+// swap
 
 #include "../ft_containers-unit-test/sources/system/system_methods.ipp"
 #include "../ft_containers-unit-test/sources/vector_tests/__service.ipp"
 
-// int _test_passed_cnt = 0;
 
-// using std::string;
-// using std::cout;
-// using std::endl;
+template <typename T>
+std::vector<int> swap_test(std::vector<T> vector) {
+    std::vector<int> v;
+    vector.assign(1100 * _ratio, 11);
+    std::vector<int> tmp(500 * _ratio, 5), tmp2(1000 * _ratio, 10), tmp3(1500 * _ratio, 15), tmp4(3000 * _ratio, 30);
+    g_start1 = timer();
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    long *adr1 = reinterpret_cast<long *>(&vector);
+    long *adr2 = reinterpret_cast<long *>(&tmp);
+    vector.swap(tmp);
+    if (reinterpret_cast<long *>(&vector) == adr1 && reinterpret_cast<long *>(&tmp) == adr2)
+    	v.push_back(1);
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    vector.swap(tmp3);
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    std::swap(vector, tmp2);
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    std::swap(vector, tmp4);
+    g_end1 = timer();
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    std::cout << v.size() << std::endl;
+    std::cout << v.capacity() << std::endl;
+    return v;
+}
 
-// const std::string GREEN = "\x1B[1;32m";
-// const std::string REDD = "\x1B[1;31m";
-// const std::string YELLOW = "\x1B[1;33m";
-// const std::string WHITE = "\x1B[1;39m";
-// const std::string RESET = "\033[0m";
+template <typename T>
+std::vector<int> swap_test(_vector<T> vector) {
+    std::vector<int> v;
+    vector.assign(1100 * _ratio, 11);
+    _vector<int> tmp(500 * _ratio, 5), tmp2(1000 * _ratio, 10), tmp3(1500 * _ratio, 15), tmp4(3000 * _ratio, 30);
+    g_start2 = timer();
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    long *adr1 = reinterpret_cast<long *>(&vector);
+    long *adr2 = reinterpret_cast<long *>(&tmp);
+    vector.swap(tmp);
+    if (reinterpret_cast<long *>(&vector) == adr1 && reinterpret_cast<long *>(&tmp) == adr2)
+    	v.push_back(1);
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    vector.swap(tmp3);
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    std::swap(vector, tmp2);
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    std::swap(vector, tmp4);
+    g_end2 = timer();
+    v.push_back(vector[2]);
+    v.push_back(vector.size());
+    v.push_back(vector.capacity());
+    std::cout << v.size() << std::endl;
+    std::cout << v.capacity() << std::endl;
+    return v;
+}
 
-// void printElement(std::string t)
-// {
-// 	if (t == "OK")
-// 	    t = GREEN + t + RESET;
-// 	else if (t == "FAILED") t = REDD + t + RESET;
-// 	cout << std::left << std::setw(30) << std::setfill(' ') << t;
-// }
+int main() {
 
-// template <class T, class A>
-// int run_vector_allocator_unit_test(std::string test_name, void (func)(ft::vector<T, A>))
-// {
-//     ft::vector<T, A> my_vector;
-
-//     printElement(test_name);
-//     func(my_vector);
-//     if (_allocator_used) {
-//         printElement("OK");
-//         cout << endl;
-//         return (0);
-//     }
-//     else {
-//         printElement("FAILED");
-//         cout << endl;
-//         return (1);
-//     }
-// }
-
-
-template <class T, class A>
-void allocator_test(ft::vector<T, A> vector)
-{
-    for (int i = 0; i < 10; ++i) {
-        vector.push_back(i);
-    }
-};
-
-int main()
-{
-    exit(run_vector_allocator_unit_test<int, Alloc<int> >("using allocator", allocator_test));
+    exit(run_vector_unit_test<int>("swap()", swap_test, swap_test));
 }
