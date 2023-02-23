544d9b8fc4fb304ddc581d735abf4da1bbedc745
eunsiho <versailles@korea.ac.kr>
2023-02-23 13:37:03 +0900

    1

--- containers_test/srcs/map/ite_n1.cpp
+++ containers_test/srcs/map/ite_n1.cpp
@@ -1,12 +1,13 @@
 #include "common.hpp"
+#include <map>
 
 #define T1 int
 #define T2 int
 
 int		main(void)
 {
-	TESTED_NAMESPACE::map<T1, T2> const mp;
-	TESTED_NAMESPACE::map<T1, T2>::iterator it = mp.begin(); // <-- error expected
+	std::map<T1, T2> const mp;
+	std::map<T1, T2>::iterator it (mp.begin()); // <-- error expected
 
 	(void)it;
 	return (0);
